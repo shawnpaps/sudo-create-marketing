1be2f110cf85e00c4c0d87bc0d7ba32c5b3daf61
Shawn <spapineau@spaptechnology.com>
2026-05-19 19:00:35 -0400

    payload is working

---
+++ cms/src/payload.config.ts
@@ -0,0 +1,62 @@
+import { s3Storage } from '@payloadcms/storage-s3'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+        // 'media-with-prefix': {
+        //   prefix,
+        // },
+        // 'media-with-presigned-downloads': {
+        //   // Filter only mp4 files
+        //   signedDownloads: {
+        //     shouldUseSignedURL: ({ collection, filename, req }) => {
+        //       return filename.endsWith('.mp4')
+        //     },
+        //   },
+        // },
+      },
+      bucket: process.env.S3_BUCKET,
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+        region: process.env.S3_REGION,
+        // ... Other S3 configuration
+      },
+    }),
+  ],
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL || '',
+    },
+  }),
+  sharp,
+  plugins: [],
+})
